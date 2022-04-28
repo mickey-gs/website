de6acf74bd45793741852c6da04f478521bcad7b
Michael <mgirvan838@gmail.com>
2022-04-28 21:00:20 +0100

    basic presentation of content

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,22 @@
 class HomepageController < ApplicationController
   def index
-    @education = Education.all
-    @experience = Experience.all
+    @education = Education.all.sort do |a, b|
+      if a.received_date.nil?
+        b.received_date.nil? ? 0 : 1
+      elsif b.received_date.nil?
+        1
+      else
+        b.received_date <=> a.received_date
+      end
+    end
+    @experience = Experience.all.sort do |a, b|
+      if a.start_date.nil?
+        b.start_date.nil? ? 0 : 1
+      elsif b.start_date.nil?
+        1
+      else
+        b.start_date <=> a.start_date
+      end
+    end
   end
 end
